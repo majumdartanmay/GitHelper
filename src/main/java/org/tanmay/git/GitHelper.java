a6de43ba52d4ead5f6363eea46e0266f106e1293
majumdartanmay <tanmay.majumdar@hotmail.com>
2024-03-25 17:36:36 +0530

    Handle pull messaging

--- src/main/java/org/tanmay/git/GitHelper.java
+++ src/main/java/org/tanmay/git/GitHelper.java
@@ -1,7 +1,6 @@
 package org.tanmay.git;
 
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.PushCommand;
+import org.eclipse.jgit.api.*;
 import org.eclipse.jgit.transport.PushResult;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import java.io.File;
@@ -43,8 +42,7 @@ public class GitHelper {
     private void push() throws Exception {
         try (Git git  = Git.open(new File(path))) {
             PushCommand pushCommand = git.push();
-            pushCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider(username, password));
-            for (PushResult result : pushCommand.call()) {
+            for (PushResult result : processTransport(pushCommand))  {
                 result.getRemoteUpdates().forEach(System.out::println);
             }
         }
@@ -52,14 +50,18 @@ public class GitHelper {
 
     private void pull() throws Exception {
         try (Git git  = Git.open(new File(path))) {
-            PushCommand pushCommand = git.push();
-            pushCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider(username, password));
-            for (PushResult result : pushCommand.call()) {
-                result.getRemoteUpdates().forEach(System.out::println);
-            }
+            PullCommand command = git.pull();
+            PullResult result = processTransport(command);
+            result.getFetchResult().submoduleResults().forEach(
+                    (key, value) -> System.out.printf("FETCHED RESULT : %s, %s%n", key , value.getMessages()));
+            System.out.printf("MERGE RESULTS %s", result.getMergeResult().getMergeStatus());
         }
     }
 
+    private <T extends TransportCommand<T, Y>, Y> Y processTransport(TransportCommand<T, Y> command) throws Exception {
+        command.setCredentialsProvider(new UsernamePasswordCredentialsProvider(username, password));
+        return command.call();
+    }
     private static boolean isEmpty(final String st) {
         return st == null || st.isEmpty();
     }
